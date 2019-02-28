b18025f04a68099d5d589dad96ca11ab85ba2fe9
Sweetysweet <gsm_v@mail.ru>
2019-02-28 18:59:29 +0300

    first commit

---
+++ mockapi/db.js
@@ -0,0 +1,13 @@
+// const rand = require('./helpers/index.js');
+
+module.exports = () => {
+  return {
+<<<<<<< HEAD
+    callback: []
+=======
+    callback: [],
+    subscribe: [],
+    // number: rand.boolean()
+>>>>>>> origin/vue
+  };
+};
